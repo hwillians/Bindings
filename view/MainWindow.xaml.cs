e19ed694ff322a5b6afa6b8d89b27b3ce8566ab7
hwillians <66992569+hwillians@users.noreply.github.com>
2021-05-11 22:12:07 +0200

    getcontent

--- view/MainWindow.xaml.cs
+++ view/MainWindow.xaml.cs
@@ -15,17 +15,22 @@ namespace view
 			InitializeComponent();
 		}
 
+		void GetContent(UserControl userControl)
+		{
+			view.Content = userControl;
+		}
+
 		private void MenuItem_Click(object sender, RoutedEventArgs e)
 		{
 			var item = (MenuItem)sender;
 			var id = item.Uid;
 			switch (id)
 			{
-				case "0": view.Content = new TestString(); break;
-				case "1": view.Content = new TestListString(); break;
-				case "2": view.Content = new TestMessages(); break;
-				case "3": view.Content = new UserControlRandom(); break;
-				case "4": view.Content = new UserControlMessageTemplate(); break;
+				case "0": GetContent(new TestString()); break;
+				case "1": GetContent(new TestListString()); break;
+				case "2": GetContent(new TestMessages()); break;
+				case "3": GetContent(new UserControlRandom()); break;
+				case "4": GetContent(new UserControlMessageTemplate()); break;
 				default: break;
 			}
 		}
